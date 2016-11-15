2d57aaf4c081d6412d4743dbaf88b40341432158
Hyun Frantz <hyunfrantz@gmail.com>
2016-11-15 17:03:21 -0500

    nmbr Adventure Mode

--- guessing_game.rb
+++ guessing_game.rb
@@ -4,36 +4,41 @@ def pick_number
 end
 
 def winning_message
-  puts "✨✨✨ Wow, you rock!! 🍻🍻🍻"
+  puts "✨ ✨ ✨ Wow, you rock!! 🍻 🍻 🍻"
 end
 
 i=1
 num = 5
 random = rand(1..100)
+numbers = []
 
 until i > num do
   number = pick_number
+  if numbers.include?(number)             #checking if the number user just entered exist in array.
+    puts "What is wrong with you? 😤 😤 😤"  #To make it appear right after typing a new number,
+  end                                     #place it right under user input
 
-  #puts "Pick your random number"
-  #number = gets.chomp
-  #number = number.to_i
-  puts "CHEAT MODE: #{random}"  #<-- Good practice to check the code
+  numbers << number
+  #puts "CHEAT MODE: #{random}"  #<-- Good practice to check the code
   if number == random
     winning_message
     break
   end
 
   case
-  when number < random
-    puts "You were too low"
-  when number > random
-    puts "You are too high"
-  else
-    puts "Try again!"
+    when number < random
+      puts "You were too low"
+    when number > random
+      puts "You are too high"
+    else
+      puts "Try again!"
   end
   i += 1
 end
 
 if i >= num # if i == num, it won't put out puts because the condition is to loop until i>num
-  puts "😵😵 You lose 😵😵!"
+  puts "😵 😵 You lose 😵 😵!"
 end
+
+
+#p numbers
